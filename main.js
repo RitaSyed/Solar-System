7b977168b32ff4091af5e37915d1e79d09961816
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-29 21:10:27 -0500

    can click on card's image and console log out 'card clicked'

--- main.js
+++ main.js
@@ -18,20 +18,18 @@ const buildDomString = (planets) => {
 
 const addEventListenersHeading = () => {
   const planetCardHeading = document.getElementsByClassName("heading");
-  // console.log(planetCardHeading);
   for(let i=0; i<planetCardHeading.length; i++){
-    // console.log(planetCardHeading);
     planetCardHeading[i].addEventListener('mouseover', cardEvents);
-    // console.log(planetCardHeading[i]);
   }
 };
-// addEventListener();
+
 const cardEvents = (e) => {
-  headdingDissapear(e);
+  headingDissapear(e);
   displayImage(e);
+  // cardClicked();
 };
 
-const headdingDissapear = (e) => {
+const headingDissapear = (e) => {
   const card = e.target.parentNode;
   e.target.style.display = "none";
 }
@@ -42,16 +40,45 @@ const displayImage = (e) => {
   img.classList.add("displayImg");
   }
 
+const cardClicked = () => {
+  // console.log("card clicked");
+   const card = document.getElementsByTagName("img");
+ 
+  //  const card = document.getElementsByClassName("card");
+  const cardsParent = card.parentNode;
+
+  // console.log(cardsParent);
+  for(let i=0; i<card.length; i++){
+    card[i].addEventListener('click', displayClickedCard);
+    console.log(card[i]);
+    
+  }
+};
 
 
+const displayClickedCard = (e) => {
+  //  const planetCardHeading = document.getElementsByClassName("heading");
+//  e.target.remove();
+  console.log("card clicked");
+
+}
+
+ 
+// const info = (info) => {
+//   for(let i=0; i<info.length; i++){
+//   return info[i];
+//   }
+// }
 
 
 function executeThisCodeAfterFileLoaded () {
   const data = JSON.parse(this.responseText);
   buildDomString(data.planets);
   addEventListenersHeading();
+  cardClicked ();
   // console.log(data);
-  // console.log(data.planets);
+  // console.log();
+  // info (data.planets);
 }
 
 
@@ -59,6 +86,7 @@ function executeThisCodeIfXHRFails () {
   console.log("error");
 }
 
+// console.log
 const startApplication = () => {
   let myRequest = new XMLHttpRequest();
   myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
