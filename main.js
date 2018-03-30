c6eceb77abd738009ca52861b6197f5775ffef71
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-29 19:21:56 -0500

    planet image appears instead of planet heading when mouse moves over the heading

--- main.js
+++ main.js
@@ -9,15 +9,47 @@ const buildDomString = (planets) => {
   planets.forEach((planet) => {
     domString += `<div class="card">`;
     domString +=    `<h1 class="heading">${planet.name}</h1>`;
+    domString +=    `<img class="image-hidden" src="${planet.imageUrl}">`;
     domString += `</div>`;
   })
   // console.log(domString);
   printToDom(domString, "planets-holder");
 };
 
+const addEventListenersHeading = () => {
+  const planetCardHeading = document.getElementsByClassName("heading");
+  // console.log(planetCardHeading);
+  for(let i=0; i<planetCardHeading.length; i++){
+    // console.log(planetCardHeading);
+    planetCardHeading[i].addEventListener('mouseover', cardEvents);
+    // console.log(planetCardHeading[i]);
+  }
+};
+// addEventListener();
+const cardEvents = (e) => {
+  headdingDissapear(e);
+  displayImage(e);
+};
+
+const headdingDissapear = (e) => {
+  const card = e.target.parentNode;
+  e.target.style.display = "none";
+}
+
+const displayImage = (e) => {
+  const img = e.target.parentNode.querySelector('.image-hidden');
+  img.classList.remove("image-hidden");
+  img.classList.add("displayImg");
+  }
+
+
+
+
+
 function executeThisCodeAfterFileLoaded () {
   const data = JSON.parse(this.responseText);
   buildDomString(data.planets);
+  addEventListenersHeading();
   // console.log(data);
   // console.log(data.planets);
 }
